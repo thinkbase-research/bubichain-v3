180132e69890aebdfa0d85bfc2d0a0f344422318
zhaozhengyong <zhaozhengyong@bubi.cn>
2017-08-29 16:38:04 +0800

    Delete unused file

--- coverage.sh
+++
@@ -1,6 +0,0 @@
-make clean;
-find -name "*.gcda" -exec rm \{\} \; 
-find -name "*.gcno" -exec rm \{\} \;
-find -name "app.info" -exec rm \{\} \;
-
-make CXXFLAGS="-ftest-coverage -fprofile-arcs"  LDFLAGS=-fprofile-arcs;
