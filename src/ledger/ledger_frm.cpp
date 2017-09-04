d16c686b04bc192b3d5f5dae05fe0f12f4566175
qiaoxiaorui <qiaoxiaorui@bubi.cn>
2017-09-04 12:47:45 +0800

    去掉Environment写时复制的功能，减少出错风险

--- src/ledger/ledger_frm.cpp
+++ src/ledger/ledger_frm.cpp
@@ -121,10 +121,11 @@ namespace bubi {
 	{
 		value_ = std::make_shared<protocol::ConsensusValue>(request);
 		uint32_t success_count = 0;
-		environment_ = std::make_shared<Environment>();
+		environment_ = std::make_shared<Environment>(nullptr);
 
 		for (int i = 0; i < request.txset().txs_size(); i++) {
 			auto txproto = request.txset().txs(i);
+			
 			TransactionFrm::pointer tx_frm = std::make_shared<TransactionFrm>(txproto, environment_);
 			LedgerManager::Instance().transaction_stack_.push(tx_frm);
 
@@ -161,7 +162,14 @@ namespace bubi {
 
 	bool LedgerFrm::Commit(KVTrie* trie) {
 		auto batch = trie->batch_;
-		for (auto it = environment_->entries_.begin(); it != environment_->entries_.end(); it++) {
+		for (auto it = environment_->entries_.begin(); it != environment_->entries_.end(); it++){
+			std::shared_ptr<AccountFrm> account = it->second;
+			account->UpdateHash(batch);
+			std::string ss = account->Serializer();
+			std::string index = utils::String::HexStringToBin(it->first);
+			trie->Set(index, ss);
+		}
+	/*	for (auto it = environment_->entries_.begin(); it != environment_->entries_.end(); it++) {
 			auto &r = it->second;
 			switch (r.action_) {
 			case utils::ChangeAction::DEL:{
@@ -180,7 +188,7 @@ namespace bubi {
 			default:
 				break;
 			}
-		}
+		}*/
 		return true;
 	}
 }
